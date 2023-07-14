92fb72d1bc1a772cd9946a03b50592e9d0673537
Ammar <ammar.hajbakour@gmail.com.com>
2023-07-14 21:06:17 +0300

    use venv

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,12 @@
 python3 --version
 which python3
 
+npm install --production --no-cache --force
+python3 -m pip install --upgrade pip
+python3 -m venv myenv
+source myenv/bin/activate
+
 export PATH="$(which python3):/opt/render/.local/bin:$(python3 -m site --user-site):$PATH"
 echo $PATH
 
-npm install --production --no-cache --force
-python3 -m pip install --upgrade pip
 pip3 install spleeter tensorflow
-python3 -m site --user-site
