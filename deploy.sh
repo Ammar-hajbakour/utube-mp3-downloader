c9f605d93b73509dfb393ad205d6b06216392956
Ammar <ammar.hajbakour@gmail.com.com>
2023-07-14 20:31:22 +0300

    .

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@
 echo 'export PATH=$PATH:/opt/render/.local/bin'  >> ~/.bash_profile
 setenv PATH $PATH:/opt/render/.local/bin
 
+echo $PATH
+
 npm install --production --no-cache --force
 pip3 install spleeter tensorflow
