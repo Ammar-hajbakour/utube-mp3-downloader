151b8a3b3a5eef0b61d4a7ea41087b72e76f72d6
Ammar <ammar.hajbakour@gmail.com.com>
2023-07-14 20:46:23 +0300

    upgrade pip3

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export PATH="/opt/render/.local/bin:adata>=4.4 in /usr/local/lib/python3.7/dist-packages:$PATH"
+export PATH="/opt/render/.local/bin:/opt/render/.local/lib/python3.7/site-packages:$PATH"
 echo $PATH
 
 npm install --production --no-cache --force
+python3 -m pip install --upgrade pip
 pip3 install spleeter tensorflow
 python3 -m site --user-site
-
