9ea4fff979cf4c7e02ab054bb065cb63ce623346
Ammar <ammar.hajbakour@gmail.com.com>
2023-07-14 20:56:58 +0300

    update PATH

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-export PATH="/opt/render/.local/bin:/opt/render/.local/lib/python3.7/site-packages:$PATH"
+python3 --version
+which python3
+
+export PATH="$(which python3):/opt/render/.local/bin:$(python3 -m site --user-site):$PATH"
 echo $PATH
 
 npm install --production --no-cache --force
 python3 -m pip install --upgrade pip
 pip3 install spleeter tensorflow
 python3 -m site --user-site
-python3 --version
